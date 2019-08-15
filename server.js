67b41828738f83fe5c3f9b6b9d933c99108d99e1
Colton Kayser <coltonkayser@gmail.com>
2019-08-15 15:54:52 -0400

    bash profile setup

---
+++ server.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+const PROJECT3_DB = process.env.PROJECT3_DB;
+
+
+
+
+
+mongoose.connect(PROJECT3_DB ,  { useNewUrlParser: true});
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected'));
