59886e0d310ef91431637d2e36fa2f8ae61a9e8b
Colton Kayser <coltonkayser@gmail.com>
2019-08-15 20:34:57 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
 const app = angular.module('ParkApp', []);
 
 app.controller('MainController', ['$http', function($http) {
-  this.baseURL = 'http://developer.nps.gov/api/v1';
+  this.baseURL = 'https://developer.nps.gov/api/v1';
+  this.parkCode = 'parks?parkCode=acad&'
+  this.apikey = 'api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb'
+  this.
 
+
+  https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb
 }]);
