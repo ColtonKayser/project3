ea4e26b1fe635748b85683676fa253bc50332738
kristiroby@gmail.com <kristiroby@gmail.com>
2019-08-16 10:26:44 -0500

    api is hard

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,14 @@ const app = angular.module('ParkApp', []);
 app.controller('MainController', ['$http', function($http) {
   this.baseURL = 'http://developer.nps.gov/api/v1/';
   this.parks = 'parks?';
+  this.limit = 'limit=150&';
+  this.state = 'stateCode=';
+  this.stateCode = '';
   this.parksCode = 'parkCode=';//we may not need this! not including
   this.query = ''; // not including for now
   this.apiKey = '&api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb';
-  this.searchURL = this.baseURL + this.parks + this.apiKey;
-
+  this.searchURL = this.baseURL + this.parks + this.state + this.stateCode + this.limit + this.apiKey;
+  console.log(this.stateCode)
   // console.log(this.searchURL);
 
   this.getParks = () => {
@@ -17,11 +20,19 @@ app.controller('MainController', ['$http', function($http) {
       url: this.searchURL
     }).then(response => {
       console.log('after');
-      console.log(response.data.data);
+      let parksArray = response.data.data;
+      // console.log(response.data)
+      console.log(parksArray)
+      console.log(this.searchURL)
+      
+      // for (let i=0; i < parksArray.length; i++) {
+      //   if (parksArray[i].states === this.stateCode) {
+      //     console.log(parksArray[i])
+      //   }
+      // }
     })
   }//end get parks func
-
-  console.log(this.getParks());
+  // && parksArray[i].designation === 'National Park'
 }]);
 
 // https://developer.nps.gov/api/v1/parks?&api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb
