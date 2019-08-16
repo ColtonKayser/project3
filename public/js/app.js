d196624851dcda6fa9e34366f4f2448915924952
Colton Kayser <coltonkayser@gmail.com>
2019-08-16 16:07:50 -0400

    divide and conquer commit

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,9 @@ app.controller('MainController', ['$http', function($http) {
       url: this.baseURL + this.parks + this.stateCode + this.state +  this.apiKey
     }).then(response => {
         console.log('after');
-        this.parks = response.data.data;
+        console.log(response.data.data);
+        this.parksData = response.data.data;
+        // console.log(this.parks);
     })
   }// end get parks func
 
