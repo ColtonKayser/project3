d8f76e62784e8c44e4a02fcf32219f97a4146e1d
Colton Kayser <coltonkayser@gmail.com>
2019-08-16 11:57:18 -0400

    api can show results on browser

--- public/js/app.js
+++ public/js/app.js
@@ -3,33 +3,30 @@ const app = angular.module('ParkApp', []);
 app.controller('MainController', ['$http', function($http) {
   this.baseURL = 'http://developer.nps.gov/api/v1/';
   this.parks = 'parks?';
-  this.limit = 'limit=150&';
-  this.state = 'stateCode=';
-  this.stateCode = '';
-  this.parksCode = 'parkCode=';//we may not need this! not including
-  this.query = ''; // not including for now
+  this.stateCode = 'stateCode=';
+  this.state = '';
   this.apiKey = '&api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb';
-  this.searchURL = this.baseURL + this.parks + this.state + this.stateCode + this.limit + this.apiKey;
-  console.log(this.stateCode)
+  this.searchURL = this.baseURL + this.parks + this.stateCode + this.state +  this.apiKey;
+  // this.baseURL = 'http://developer.nps.gov/api/v1/';
+  // this.parks = 'parks?';
+  // this.limit = 'limit=150&';
+  // this.state = 'stateCode=';
+  // this.stateCode = '';
+  // this.parksCode = 'parkCode=';//we may not need this! not including
+  // this.query = ''; // not including for now
+  // this.apiKey = '&api_key=dIyH9p8twcw6BsTtQoTUfdOgCKnNgCVfsCAsNGhb';
+  // this.searchURL = this.baseURL + this.parks + this.state + this.stateCode + this.limit + this.apiKey;
+  // console.log(this.stateCode)
   // console.log(this.searchURL);
 
   this.getParks = () => {
     console.log('before');
     $http({
       method: 'GET',
-      url: this.searchURL
+      url: this.baseURL + this.parks + this.stateCode + this.state +  this.apiKey
     }).then(response => {
-      console.log('after');
-      let parksArray = response.data.data;
-      // console.log(response.data)
-      console.log(parksArray)
-      console.log(this.searchURL)
+        this.parks = response.data.data;
       
-      // for (let i=0; i < parksArray.length; i++) {
-      //   if (parksArray[i].states === this.stateCode) {
-      //     console.log(parksArray[i])
-      //   }
-      // }
     })
   }//end get parks func
   // && parksArray[i].designation === 'National Park'
