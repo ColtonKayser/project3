e14e8c37715e195629f92896008cebddea25e6e3
Alex <ahawknj15@gmail.com>
2019-08-18 11:33:56 -0400

    partials working

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,5 @@
 const app = angular.module('ParkApp', []);
 
-// === PARTIALS CONTROLLER === //
-app.controller('PartialsController', ['$http', function($http) {
-  console.log('partials controller');
-}]);
-
-
 
 // === API CONTROLLER === //
 app.controller('MainController', ['$http', function($http) {
